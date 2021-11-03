ea991c3bc52a47c89d9e6b1d8dcf92cbd54258c3
umangU <72771903+umangU@users.noreply.github.com>
2021-11-03 23:52:29 +1100

    Code Updated

--- Propensity-Model/model.R
+++ Propensity-Model/model.R
@@ -8,7 +8,7 @@ loan  <- read.csv("dummy.csv")
 # Replacing missing values with 0 
 loan[is.na(loan)]<-0
 
-#Scaling the numerical columns to perform SVM
+# Scaling the numerical columns to perform SVM
 loan[,c("LoanBalance","ArrearsBalance","ArrearsDays","RepaymentAmount","LVR",
         "SecurityValuation","RelationshipBalance","SavingsBalance","SavingsBalanceOne","SavingsBalanceThree","ArrearsBalanceOne",
         "ArrearsBalanceThree","ArrearsDaysOne","ArrearsDaysThree","TransactionQuantityOne","TransactionValueOne","TransactionQuantityThree","TransactionValueThree","AppUsageOne",
