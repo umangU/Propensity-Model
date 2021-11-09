52827107bd866f7455b175a31e0ace049dd539e2
umangU <72771903+umangU@users.noreply.github.com>
2021-11-09 23:50:26 +1100

    Code Updated

--- SQLMachineLearning/InstallAndConfiguration.sql
+++ SQLMachineLearning/InstallAndConfiguration.sql
@@ -4,11 +4,11 @@ EXEC sp_configure
 /*Show external scripts enabled config value*/
 EXEC sp_configure 'external scripts enabled'
 
-/*Enable external scripts (if needed)*/
+/* Enable external scripts (if needed)*/
 EXEC sp_configure 'external scripts enabled', 1
 RECONFIGURE WITH OVERRIDE
 
-/*Verify R is enabled*/
+/* Verify R is enabled*/
 EXEC sp_execute_external_script
 	@language=N'R',
 	@script=N'OutputDataSet <- InputDataSet;',
